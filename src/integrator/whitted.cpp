8ee46e2e6c6958ac318c10cdc19135e6d24eef72
Antoine Tran <antoine2001.tran@gmail.com>
2024-01-31 22:15:09 +0100

    Remove useless clipping

--- src/integrator/whitted.cpp
+++ src/integrator/whitted.cpp
@@ -11,33 +11,26 @@ public:
         /* No parameters */
     }
 
-    static Color3f clip(Color3f i) {
-        return Color3f(std::clamp(i.x(), 0.0f, 1.0f), std::clamp(i.y(), 0.0f, 1.0f), std::clamp(i.z(), 0.0f, 1.0f));
-    }
-
     Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
         /* Find the surface that is visible in the requested direction */
         Intersection its;
         
-        if (!scene->rayIntersect(ray, its))
-            return Color3f(0.0f);
-
-        if(its.mesh->getEmitter()) {
+        if (!scene->rayIntersect(ray, its)) {
             return Color3f(0.0f);
         }
 
         const BSDF* bsdf = its.mesh->getBSDF();
+        Frame frame = its.shFrame;
 
         if(bsdf->isDiffuse()) {
-            Color3f radiance;
 
             Mesh* random_emitter = scene->pickMeshEmitter();
 
             if(random_emitter) {
-                radiance += random_emitter->getEmitter()->sampleRadiance(its.mesh->getBSDF(), its.p, its.shFrame.n, -ray.d, *sampler, scene);
+                return random_emitter->getEmitter()->sampleRadiance(its.mesh->getBSDF(), its.p, frame.n, -ray.d, *sampler, scene);
             }
 
-            return clip(radiance);
+            return Color3f(0.0f);
 
         } else {
             float eps = sampler->next1D();
@@ -45,10 +38,10 @@ public:
                 return Color3f(0.0f);
             }
 
-            BSDFQueryRecord record(its.shFrame.toLocal(-ray.d));
+            BSDFQueryRecord record(frame.toLocal(-ray.d));
             Color3f sampled_value = bsdf->sample(record, sampler->next2D());
 
-            return clip(1 / 0.95 * sampled_value * Li(scene, sampler, Ray3f(its.p, its.shFrame.toWorld(record.wo)))); 
+            return 1 / 0.95 * sampled_value * Li(scene, sampler, Ray3f(its.p, frame.toWorld(record.wo))); 
         }
         
 
