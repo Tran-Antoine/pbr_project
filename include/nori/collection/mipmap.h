c17229672fcebb126f4d5f542a7bbe2795d7911e
Tran-Antoine <antoine2001.tran@gmail.com>
2024-03-02 17:33:31 +0100

    Fix warping issues (warptest not working)

--- include/nori/collection/mipmap.h
+++ include/nori/collection/mipmap.h
@@ -20,10 +20,10 @@ public:
 
     MipMap(const std::string &path, const std::string &ext, bool norm);
 
-    void h_distribution(uint8_t depth, Point2i corner, bool up, float &left, float &right) const;
-    void v_distribution(uint8_t depth, Point2i corner, float &up, float &down) const;
+    void h_distribution(Point2i corner, bool down, float &left, float &right) const;
+    void v_distribution(Point2i corner, float &up, float &down) const;
 
-    Point2f next_corner(uint8_t next_depth, Point2i previous_pos) const;
+    Point2i next_corner(uint8_t next_depth, Point2i previous_pos) const;
 
     void distribution(uint8_t depth, Point2i corner, float& left, float& right, float&up, float& down) const;
 
