b3a125c8a9eed4f27c793539f8b380ed637346be
Tran-Antoine <antoine2001.tran@gmail.com>
2024-05-01 17:26:17 +0200

    Add multitexturing with a special map

---
+++ include/nori/bsdf/multidiffusemap.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <core/object.h>
+#include <core/common.h>
+#include <core/color.h>
+#include <bsdf/diffusemap.h>
+#include <ImfRgbaFile.h>
+#include <ImfArray.h>
+
+NORI_NAMESPACE_BEGIN
+
+class MultiDiffuseMap : public DiffuseMap {
+
+public:
+    explicit MultiDiffuseMap(const PropertyList &propList);
+    Color3f T(float s, float t) const override;
+    float map(float s, int index) const;
+    void unmap(float m, float& s, int& index) const;
+
+    std::string toString() const override;
+    void addChild(nori::NoriObject *obj) override;
+
+private:
+    std::vector<DiffuseMap*> children;
+};
+
+NORI_NAMESPACE_END
---
+++ src/bsdf/multidiffusemap.cpp
@@ -0,0 +1,48 @@
+#include <core/common.h>
+#include <core/color.h>
+#include <bsdf/multidiffusemap.h>
+
+NORI_NAMESPACE_BEGIN
+
+MultiDiffuseMap::MultiDiffuseMap(const PropertyList &propList) : DiffuseMap(propList) { }
+
+Color3f MultiDiffuseMap::T(float s, float t) const {
+    int index;
+    float remapped;
+    unmap(s, remapped, index);
+
+    return children[index]->T(remapped, t);
+}
+
+std::string MultiDiffuseMap::toString() const {
+    return "multitexturemap";
+}
+
+void MultiDiffuseMap::addChild(nori::NoriObject *obj) {
+    DiffuseMap* map;
+    switch (obj->getClassType()) {
+        case NoriObject::EDiffuseMap:
+            map = static_cast<DiffuseMap *>(obj);
+            children.push_back(map);
+            break;
+        default:
+            NoriObject::addChild(obj);
+            break;
+    }
+}
+
+float MultiDiffuseMap::map(float s, int index) const {
+    float range = 1.f / children.size();
+    float start = index * range;
+    return start + s * range;
+}
+
+void MultiDiffuseMap::unmap(float m, float& s, int& index) const {
+    float range = 1.f / children.size();
+
+    index = ((int) (m / range));
+    s = m/range - index; //  (m - index * range) / range
+}
+
+NORI_REGISTER_CLASS(MultiDiffuseMap, "mtexture");
+NORI_NAMESPACE_END
