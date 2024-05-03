c31b60ae04be4794703184cced6b4c09e82916b2
Tran-Antoine <antoine2001.tran@gmail.com>
2024-05-03 18:54:58 +0200

    Add new syntax for l-system grammars

---
+++ test/parseconfigtest.cpp
@@ -0,0 +1,14 @@
+#include <parser/lsystem.h>
+#include <core/common.h>
+#include <pcg32.h>
+
+using namespace nori;
+
+
+int main(int argc, char **argv) {
+
+    auto random = pcg32();
+    auto config = LSystemGrammar::fromConfig(random, "scenes/tree2/testgrammar.l6");
+    std::cout << "Config successfully parsed\n";
+
+}
