870721a9205435263600ac66f17be5bad3e298c6
Tran-Antoine <antoine2001.tran@gmail.com>
2024-05-06 12:10:39 +0200

    Refactor turtle code

--- src/parser/turtle.cpp
+++ src/parser/turtle.cpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <parser/turtle.h>
-#include <parser/objutil.h>
 #include <parser/obj.h>
 #include <stats/warp.h>
 
@@ -11,11 +10,11 @@ int idealSmoothness(float radius) {
     return (int) (radius * 100);
 }
 
-void drawMesh(const std::string &filename, const Transform &trafo, std::vector<Vector3f> &positions,
-              std::vector<uint32_t> &indices, std::vector<Vector2f> &texcoords) {
+void drawMesh(const std::string &filename, const Transform &trafo,
+              PosVec ps, IndVec is, std::vector<Vector2f> &ts) {
 
 
-    int starting_index = positions.size();
+    int starting_index = ps.size();
 
     auto props = PropertyList();
     props.setString("filename", filename);
@@ -24,59 +23,59 @@ void drawMesh(const std::string &filename, const Transform &trafo, std::vector<V
 
     auto mesh = WavefrontOBJ(props);
 
-    auto positions_ = mesh.getVertexPositions();
-    for(int i = 0; i < positions_.cols(); i++) {
-        positions.push_back(positions_.col(i));
+    auto ps_ = mesh.getVertexPositions();
+    for(int i = 0; i < ps_.cols(); i++) {
+        ps.push_back(ps_.col(i));
     }
 
-    auto indices_ = mesh.getIndices();
-    for(int i = 0; i < indices_.cols(); i++) {
-        auto column = indices_.col(i);
-        indices.push_back(starting_index + column.x());
-        indices.push_back(starting_index + column.y());
-        indices.push_back(starting_index + column.z());
+    auto is_ = mesh.getIndices();
+    for(int i = 0; i < is_.cols(); i++) {
+        auto column = is_.col(i);
+        is.push_back(starting_index + column.x());
+        is.push_back(starting_index + column.y());
+        is.push_back(starting_index + column.z());
     }
 
-    auto texcoords_ = mesh.getVertexTexCoords();
-    for(int i = 0; i < texcoords_.cols(); i++) {
-        texcoords.push_back(texcoords_.col(i));
+    auto ts_ = mesh.getVertexTexCoords();
+    for(int i = 0; i < ts_.cols(); i++) {
+        ts.push_back(ts_.col(i));
     }
 }
 
-void drawCylinder(float length, float yaw, float pitch, float in_thickness, float out_thickness,
-                         std::vector<Vector3f> &positions, std::vector<uint32_t> &indices, std::vector<Vector2f>& texcoords, TurtleState &state) {
+void drawCylinder(TurtleState &state, PosVec ps, IndVec is, TexVec ts) {
 
     Vector3f a = state.p;
     Vector3f a_n = state.p_n;
-    Vector3f direction = directional(pitch, yaw);
-    Vector3f b = a + length * direction;
-    Vector3f b_n = (b-a).normalized();
+    Vector3f b = state.forward();
+    Vector3f b_n = state.normalOut();
+
+    float in_thick = state.in_thickness, out_thick = state.out_thickness;
 
     if(abs(a_n.dot(b_n)) < 0.5) {
         // start with the out thickness right away
-        drawStraightCylinder(a, b, out_thickness, out_thickness, positions, indices, texcoords, state);
+        drawStraightCylinder(a, b, out_thick, out_thick, ps, is, ts, state);
     }
 
-    connect(a, b, a_n, in_thickness, out_thickness, idealSmoothness(std::max(in_thickness, out_thickness)),
-            positions, indices, texcoords);
+    connect(a, b, a_n, in_thick, out_thick, idealSmoothness(std::max(in_thick, out_thick)),
+            ps, is, ts);
     state.p = b;
     state.p_n = b_n;
-    state.in_thickness = out_thickness;
+    state.in_thickness = out_thick;
 }
 
-void drawCylinder(const Point3f& a, const Point3f& b, const Vector3f& a_n, const Vector3f& b_n, float in_thickness, float out_thickness,
-                  std::vector<Vector3f> &positions, std::vector<uint32_t> &indices, std::vector<Vector2f>& texcoords, TurtleState &state) {
+void drawCylinder(const Point3f& a, const Point3f& b, const Vector3f& a_n, const Vector3f& b_n, float in_thick, float out_thick,
+                  PosVec ps, IndVec is, TexVec ts, TurtleState &state) {
 
     if(abs(a_n.dot(b_n)) < 0.5) {
-        // start with the out thickness right away
-        drawStraightCylinder(a, b, out_thickness, out_thickness, positions, indices, texcoords, state);
+        // start with the out thick right away
+        drawCylinder(a, b, b_n, b_n, out_thick, out_thick, ps, is, ts, state);
     }
 
-    connect(a, b, a_n, in_thickness, out_thickness, idealSmoothness(std::max(in_thickness, out_thickness)),
-            positions, indices, texcoords);
+    connect(a, b, a_n, in_thick, out_thick, idealSmoothness(std::max(in_thick, out_thick)),
+            ps, is, ts);
     state.p = b;
     state.p_n = b_n;
-    state.in_thickness = out_thickness;
+    state.in_thickness = out_thick;
 }
 
 static void addNoise(float& yaw, float& pitch, float sample, float variance) {
@@ -84,50 +83,49 @@ static void addNoise(float& yaw, float& pitch, float sample, float variance) {
     pitch *= 1 + Warp::lineToLogistic(sample, variance);
 }
 
-void drawCylinder(float length, float yaw, float pitch, float in_thickness, float out_thickness, int n_nodes, float variance, float sample,
-                  std::vector<Vector3f> &positions, std::vector<uint32_t> &indices, std::vector<Vector2f>& texcoords, TurtleState &state) {
+void drawCylinder(int n_nodes, float variance, float sample,
+                  PosVec ps, IndVec is, TexVec ts, TurtleState &state) {
+
+    float length = state.length;
+    float in_thick = state.in_thickness, out_thick = state.out_thickness;
+    float yaw = state.yaw, pitch = state.pitch;
     float dl = length / (float) n_nodes;
-    float dt = (out_thickness - in_thickness) / (float) n_nodes;
+    float dt = (out_thick - in_thick) / (float) n_nodes;
 
-    Vector3f end_point = state.p + length * directional(pitch, yaw);
-    Vector3f next;
+    Vector3f end_point = state.forward();
+    Vector3f end_out_normal = state.normalOut();
+    float    end_thickness = state.out_thickness;
 
-    state.in_thickness = in_thickness;
+    state.length = dl;
 
     for(int i = 0; i < n_nodes - 1; i++) {
-
+        state.out_thickness += dt;
         addNoise(yaw, pitch, sample, variance);
-        next = state.p + dl * directional(pitch, yaw);
-        drawCylinder(dl, yaw, pitch, state.in_thickness, state.in_thickness + dt, positions, indices, texcoords, state);
+        drawCylinder(state, ps, is, ts);
     }
 
-    drawCylinder(state.p, end_point, state.p_n, (end_point - next).normalized(), state.in_thickness, out_thickness, positions, indices, texcoords, state);
-
+    drawCylinder(state.p, end_point, state.p_n, end_out_normal, in_thick, end_thickness, ps, is, ts, state);
 }
 
-void drawStraightCylinder(const Point3f& a, const Point3f& b, float in_thickness, float out_thickness,
-                          std::vector<Vector3f> &positions, std::vector<uint32_t> &indices, std::vector<Vector2f>& texcoords, TurtleState &state) {
+void drawStraightCylinder(const Point3f& a, const Point3f& b, float in_thick, float out_thick,
+                          PosVec ps, IndVec is, TexVec ts, TurtleState &state) {
 
-    connect(a, b, (b-a).normalized(), in_thickness, out_thickness, idealSmoothness(std::max(in_thickness, out_thickness)),
-            positions, indices, texcoords);
-
-    state.p = b;
-    state.p_n = (b-a).normalized();
-    state.in_thickness = out_thickness;
+    Vector3f normal = (b-a).normalized();
+    drawCylinder(a, b, normal, normal, in_thick, out_thick, ps, is, ts, state);
 }
 
-void connect(const Point3f &a, const Point3f &b, const Vector3f &a_n, float in_thickness, float out_thickness,
-                    int smoothness, std::vector<Vector3f> &positions, std::vector<uint32_t> &indices,
-                    std::vector<Vector2f> &texcoords) {
+void connect(const Point3f &a, const Point3f &b, const Vector3f &a_n, float in_thick, float out_thick,
+                    int smoothness, PosVec ps, IndVec is,
+                    std::vector<Vector2f> &ts) {
 
     Vector3f b_n = (b-a).normalized(); // normal of b must be according to the cylinder's direction
 
-    std::vector<Vector3f> from_circle = circle(a, a_n, in_thickness, smoothness);
-    std::vector<Vector3f> to_circle   = circle(b, b_n, out_thickness, smoothness);
+    std::vector<Vector3f> from_circle = circle(a, a_n, in_thick, smoothness);
+    std::vector<Vector3f> to_circle   = circle(b, b_n, out_thick, smoothness);
 
-    int index_pointer = positions.size(); // save pointer before adding new vertices
-    push_circle(from_circle, a, positions, indices, texcoords, false);
-    push_circle(to_circle, b, positions, indices, texcoords, true);
+    int index_pointer = ps.size(); // save pointer before adding new vertices
+    push_circle(from_circle, a, ps, is, ts, false);
+    push_circle(to_circle, b, ps, is, ts, true);
 
     int n_points = from_circle.size() + 1; // +1 due to the center point
 
@@ -141,25 +139,25 @@ void connect(const Point3f &a, const Point3f &b, const Vector3f &a_n, float in_t
         int i1 = i0 + 1;
         int j1 = j0 + 1;
 
-        indices.push_back(i0);
-        indices.push_back(i1);
-        indices.push_back(j1);
+        is.push_back(i0);
+        is.push_back(i1);
+        is.push_back(j1);
 
-        indices.push_back(i0);
-        indices.push_back(j1);
-        indices.push_back(j0);
+        is.push_back(i0);
+        is.push_back(j1);
+        is.push_back(j0);
 
         i0++;
         j0++;
     }
 
-    indices.push_back(i0);
-    indices.push_back(index_pointer + 1);
-    indices.push_back(index_pointer + 1 + n_points);
+    is.push_back(i0);
+    is.push_back(index_pointer + 1);
+    is.push_back(index_pointer + 1 + n_points);
 
-    indices.push_back(i0);
-    indices.push_back(index_pointer + 1 + n_points);
-    indices.push_back(j0);
+    is.push_back(i0);
+    is.push_back(index_pointer + 1 + n_points);
+    is.push_back(j0);
 }
 
 std::vector<Vector3f> circle(const Vector3f& p, const Vector3f& p_n, float radius, int smoothness) {
@@ -185,45 +183,45 @@ std::vector<Vector3f> circle(const Vector3f& p, const Vector3f& p_n, float radiu
     return (pos);
 }
 
-void push_circle(std::vector<Vector3f> &circle_positions, const Vector3f &p, std::vector<Vector3f> &positions,
-                        std::vector<uint32_t> &indices, std::vector<Vector2f> &texcoords,
+void push_circle(PosVec circle_ps, const Vector3f &p, PosVec ps,
+                        IndVec is, std::vector<Vector2f> &ts,
                         bool fill_circle) {
 
-    int center_pointer = positions.size();
-    int head = positions.size() + 1;
+    int center_pointer = ps.size();
+    int head = ps.size() + 1;
     int edge_pointer = head;
 
     float y_texture = fill_circle ? 1.f : 0.f;
 
-    positions.push_back(p);
-    texcoords.push_back(Vector2f(0.5f, y_texture)); // the value of this one doesn't really matter
+    ps.push_back(p);
+    ts.push_back(Vector2f(0.5f, y_texture)); // the value of this one doesn't really matter
 
-    Vector3f edge0 = circle_positions[0];
-    positions.push_back(edge0);
-    texcoords.push_back(Vector2f(0.0f, y_texture));
+    Vector3f edge0 = circle_ps[0];
+    ps.push_back(edge0);
+    ts.push_back(Vector2f(0.0f, y_texture));
 
-    for(int i = 1; i <= circle_positions.size() - 1; i++) {
+    for(int i = 1; i <= circle_ps.size() - 1; i++) {
 
-        float x_texture = (float) i / (float) circle_positions.size();
+        float x_texture = (float) i / (float) circle_ps.size();
 
-        Vector3f edge1 = circle_positions[i];
-        positions.push_back(edge1);
-        texcoords.push_back(Vector2f(x_texture, y_texture));
+        Vector3f edge1 = circle_ps[i];
+        ps.push_back(edge1);
+        ts.push_back(Vector2f(x_texture, y_texture));
 
 
         if(fill_circle) {
-            indices.push_back(center_pointer);
-            indices.push_back(edge_pointer);
-            indices.push_back(edge_pointer+1);
+            is.push_back(center_pointer);
+            is.push_back(edge_pointer);
+            is.push_back(edge_pointer+1);
         }
 
         edge_pointer++;
     }
 
     if(fill_circle) {
-        indices.push_back(center_pointer);
-        indices.push_back(edge_pointer);
-        indices.push_back(head);
+        is.push_back(center_pointer);
+        is.push_back(edge_pointer);
+        is.push_back(head);
     }
 }
 
